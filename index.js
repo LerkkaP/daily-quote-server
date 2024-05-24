19abc7d026b238f2f8305b997eb8b6b55c60f092
Erik Peteri <peteri.erik@gmail.com>
2024-05-24 22:13:22 +0300

    remove constants.js and add axios request to quote

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const cron = require("node-cron");
-import { api_key, api_base_url, api_service_url } from "./constants";
+const axios = require("axios")
+const cron = require("node-cron")
 
 require('dotenv').config()
+
+const api_key = process.env.API_KEY;
+const api_base_url = process.env.API_BASE_URL;
 
 const app = express();
 app.use(express.json());
@@ -10,8 +13,17 @@ app.use(express.json());
 const PORT = 3000;
 
 app.get("/", (_, res) => {
-    res.send("hello world");
-  });
+  res.send("hello world");
+});
+
+app.get("/quote", async (_, res) => {
+  try {   
+    const response = await axios.get(`${api_base_url}${api_key}`)
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'There was an error in fetching the quote.' })
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
