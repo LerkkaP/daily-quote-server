53f3bf4b9e477f78bcfd15ffc01bfa4009d50b27
Erik Peteri <peteri.erik@gmail.com>
2024-05-25 18:26:00 +0300

    add basic cron structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const axios = require("axios")
 const cron = require("node-cron")
+const moment = require('moment-timezone');
 
 require('dotenv').config()
 
@@ -16,16 +17,31 @@ app.get("/", (_, res) => {
   res.send("hello world");
 });
 
+const timezone = 'America/Chicago'
+
+cron.schedule('0 0 * * *', () => {
+  const now = moment().tz(timezone);
+  if (now.hour() === 0 && now.minute() === 0) {
+    /**
+   * @todo make api request and cache the result for 24 hours
+   */
+    }
+  }, {
+    timezone 
+  });
+
 app.get("/quote", async (_, res) => {
-  try {   
-    const response = await axios.get(`${api_base_url}${api_key}`)
-    res.send(response.data)
-  } catch (error) {
-    res.status(500).json({ error: 'There was an error in fetching the quote.' })
-  }
+    try {   
+      const response = await axios.get(`${api_base_url}${api_key}`)
+      res.send(response.data)
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error in fetching the quote.' })
+    }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   
 });
+
